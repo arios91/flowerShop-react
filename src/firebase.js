822989307c2e9253f1bf4e07bc7430125e18b5a2
Alex <alx.rios91@gmail.com>
2022-04-29 02:32:57 -0500

    switched to prod environment

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ const { initializeAppCheck, ReCaptchaV3Provider, getToken } = require("firebase/
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAMiNHNKa_gzQKuivRwFydgMAZNqB8K9hs",
-    authDomain: "petalos-arte.firebaseapp.com",
-    databaseURL: "https://petalos-arte.firebaseio.com",
-    projectId: "petalos-arte",
-    storageBucket: "petalos-arte.appspot.com",
-    messagingSenderId: "220047130772",
-    appId: "1:220047130772:web:b70df6e5da9d328a643481"
-  };
+  apiKey: "AIzaSyDRv5LVfWc0icCVb0OwoUQ25jgkipjTCWU",
+  authDomain: "petalosarteprod.firebaseapp.com",
+  databaseURL: "https://petalosarteprod.firebaseio.com",
+  projectId: "petalosarteprod",
+  storageBucket: "petalosarteprod.appspot.com",
+  messagingSenderId: "1039687597381",
+  appId: "1:1039687597381:web:4fb27936c8f0d78f92522c"
+};
 
   const firebase = initializeApp(firebaseConfig);
 
